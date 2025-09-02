27c320d8ca791534e93ba978bead2a497e233997
Luiz <nandoluizprimeiro@gmail.com>
2025-09-02 14:07:16 -0300

    Criando componente About

---
+++ src/components/About/index.jsx
@@ -0,0 +1,16 @@
+import './About.css';
+
+const About = () => {
+    return (
+        <nav className="container-about">
+            <div className="saudacoes">
+                <h1>Um pouco sobre mim:</h1>
+            </div>
+            <div className='container-photo'>
+                <img className='photo' src='../public/luiz-photo.jpg'></img>
+            </div>
+        </nav>
+    );
+}
+
+export default About;
