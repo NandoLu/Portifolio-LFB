c73d776c1421a49bfaa3230e71752c071a96cf3d
Luiz <nandoluizprimeiro@gmail.com>
2025-09-02 16:06:19 -0300

    Adicionando Habilidades

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,7 +3,11 @@ import './Home.css';
 const Home = () => {
     return (
         <>
+            {/* SAUDAÇÕES  */}
+
             <nav className="section">
+
+
                 <div className="title">
                     <h1>Olá!</h1>
                     <h1>Me chamo</h1>
@@ -13,10 +17,13 @@ const Home = () => {
                     <img className='photo' src='../public/luiz-photo.jpg' alt="Foto de Luiz Fernando Balbino"></img>
                 </div>
             </nav>
+
+            {/* SOBRE  */}
+
             <nav className="section">
                 <div className="text">
                     <h1>Um pouco sobre mim</h1>
-                    <p>Sou estudante de Engenharia de Software na
+                    <p>Sou bacharelando de Engenharia de Software (2/8) na
                         FIAP, tenho 19 anos e estou buscando uma oportunidade de estágio como
                         Desenvolvedor.</p>
                     <p>Formado em Técnico em Informática em 2024 no Instituto Federal do Sul de Minas.</p>
@@ -25,6 +32,9 @@ const Home = () => {
                     <img className='icon' src='../public/icons/data-management.png' alt="Icone Tecnologia"></img>
                 </div>
             </nav>
+
+            {/* CONTRIBUICOES */}
+
             <div className="contribuicoes-container">
                 <div className="title">
                     <h1 >Minhas Contribuições</h1>
@@ -40,6 +50,44 @@ const Home = () => {
                     </p>
                 </div>
             </div>
+
+            {/* HABILIDADES */}
+
+            <nav className="section">
+
+                <div className="text">
+                    <h1>Habilidades</h1>
+                    <div>
+                        <h2>Back-End</h2>
+                        <ul>
+                            <li>Node JS</li>
+                            <li>JavaScript</li>
+                            <li>TypeScript</li>
+                            <li>Python</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2>Front-End</h2>
+                        <ul>
+                            <li>React JS</li>
+                            <li>React-Native</li>
+                            <li>JavaScript</li>
+                            <li>TypeScript</li>
+                            <li>Python</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h2>Ferramentas</h2>
+                        <ul>
+                            <li>Git</li>
+                            <li>GitHub</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
         </>
     );
 }
