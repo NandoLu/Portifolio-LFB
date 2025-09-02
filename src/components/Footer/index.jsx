df03d5fdc39ca03ff2bc766b617b0bf56a706809
Luiz <nandoluizprimeiro@gmail.com>
2025-09-02 10:46:07 -0300

    Estilizando footer e adicionando icones

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,15 +3,27 @@ import './Footer.css';
 const Footer = () => {
     return (
         <nav className="footer">
-            <ul className="footer-list">
-                <li><a href="https://wa.me/5516992322494" target="_blank">whats</a></li>
-                <li><a href="https://www.instagram.com/luiz.fernando.balbino/" target="_blank">insta</a></li>
-                <li><a href="https://www.linkedin.com/in/luiz-fernando-balbino-2336a1349/" target="_blank">linkedin</a></li>
-                <li><a href="https://github.com/NandoLu" target="_blank">github</a></li>
-                <li><a href="https://www.youtube.com/@luiz.fernando.balbino" target="_blank">yout</a></li>
-            </ul>
+
+            <div className="icons">
+                <a href="https://wa.me/5516992322494" target="_blank">
+                    <img src="../public/icons/whatsapp.png" alt="WhatsApp" />
+                </a>
+                <a href="https://www.instagram.com/luiz.fernando.balbino/" target="_blank">
+                    <img src="../public/icons/instagram.png" alt="Instagram" />
+                </a>
+                <a href="https://www.linkedin.com/in/luiz-fernando-balbino-2336a1349/" target="_blank">
+                    <img src="../public/icons/linkedin.png" alt="LinkedIn" />
+                </a>
+                <a href="https://github.com/NandoLu" target="_blank">
+                    <img src="../public/icons/github.png" alt="GitHub" />
+                </a>
+                <a href="https://www.youtube.com/@luiz.fernando.balbino" target="_blank">
+                    <img src="../public/icons/youtube.png" alt="YouTube" />
+                </a>
+            </div>
+
             <div className="logo">
-                <p>Desenvolvido por Luiz Fernando Balbino &copy; 2025</p>
+                <p>Desenvolvido por Luiz Fernando Balbino © 2025</p>
             </div>
         </nav>
     );
