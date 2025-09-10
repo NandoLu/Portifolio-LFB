496f0c6e4866e4253c21159fbc8825ac42f8e587
Luiz <nandoluizprimeiro@gmail.com>
2025-09-10 20:44:17 -0300

    Style: Ajustes pequenos

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,9 +4,14 @@ const About = () => {
     return (
         <nav className="section">
             <div className="saudacoes">
-                <h1>Um pouco sobre mim:</h1>
+                <h1>Quem sou eu?</h1>
+                <h1>Estudante de Engenharia de Software</h1>
+                <p className="font-normal text-lg md:text-xl m-0">
+                    Sou bacharelando de Engenharia de Software (2/8) na FIAP, tenho 19 anos e estou buscando uma oportunidade de estágio como Desenvolvedor.
+                </p>
             </div>
             <div className='container-photo'>
+                <p></p>
                 <img className='photo' src='../public/luiz-photo.jpg'></img>
             </div>
         </nav>
