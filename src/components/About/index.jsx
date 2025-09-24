211cb9b4f557ad2d8f6a722a8ae401e8966f86a3
Luiz <nandoluizprimeiro@gmail.com>
2025-09-24 14:34:35 -0300

    Finalizando o componente About

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,6 +1,8 @@
+import Habilidades from "../Habilidades";
+
 const About = () => {
     return (
-        <section className="flex flex-col md:flex-row flex-1 justify-around items-center text-white w-full py-20 px-4 md:px-12 box-border">
+        <section className="flex flex-col flex-1 justify-around items-center text-white w-full py-20 px-4 md:px-12 box-border">
             <div className="flex flex-col w-full md:w-1/2 p-0 text-center md:text-left">
                 <h1 className="text-3xl md:text-5xl font-bold m-0 md:m-1">Quem sou eu?</h1>
                 <h1 className="text-3xl md:text-5xl font-bold m-0 md:m-1">Estudante de Engenharia de Software</h1>
@@ -8,9 +10,12 @@ const About = () => {
                     Sou bacharelando de Engenharia de Software (2/8) na FIAP, tenho 19 anos e estou buscando uma oportunidade de estágio como Desenvolvedor.
                 </p>
             </div>
-            <div className="w-full md:w-1/4 mt-8 md:mt-0 flex justify-center">
-                <img className="rounded-full w-full max-w-sm h-auto" src='../public/luiz-photo.jpg' alt="Foto de Luiz Fernando Balbino" />
-            </div>
+            {/* HABILIDADES */}
+            <section className="flex flex-col items-center text-white w-full py-20 px-4 md:px-12 box-border">
+                <div className="flex flex-col items-center w-full p-0 gap-6">
+                    <Habilidades />
+                </div>
+            </section>
         </section>
     );
 }
