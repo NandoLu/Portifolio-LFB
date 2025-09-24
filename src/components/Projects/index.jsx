0da3d2d1e88afaab6e03817d64d1a0e15defe9be
Luiz <nandoluizprimeiro@gmail.com>
2025-09-24 15:53:26 -0300

    Style: Adicionando texto e alinhando ao centro

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,9 +4,12 @@ const Projects = () => {
     return (
         <>
             <section className="flex flex-col flex-1 justify-around items-center text-white w-full py-20 px-4 md:px-12 box-border">
-                <div className="flex flex-col w-full md:w-1/2 p-0 text-center md:text-left">
+                {/* Contêiner do Título e Subtítulo */}
+                <div className="flex flex-col w-full p-0 text-center">
                     <h1 className="text-3xl md:text-5xl font-bold m-0 md:m-1">Projetos</h1>
+                    <p className="text-xl md:text-2xl text-white mt-2">Aqui contém projetos acadêmicos, profissionais e pessoais! 😊</p>
                 </div>
+                
                 {/* PROJETOS */}
                 <section className="flex flex-col items-center text-white w-full py-20 px-4 md:px-12 box-border">
                     <div className="flex flex-col items-center w-full md:w-4/5 lg:w-11/12 p-0 gap-6">
