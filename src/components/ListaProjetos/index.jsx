d2a87343e6636ddef2720deb38e7d52b692aca3d
Luiz <nandoluizprimeiro@gmail.com>
2025-09-28 09:42:48 -0300

    Importando as informações do ProjectDetail

--- src/components/ListaProjetos/index.jsx
+++ src/components/ListaProjetos/index.jsx
@@ -1,33 +1,37 @@
-import educochain from '../../assets/project-img/educochain.png';
-import safewater from '../../assets/project-img/safewater.png';
-import galeriavirtual from '../../assets/project-img/galeriavirtual.png';
+import { Link } from 'react-router-dom';
+import { projectsData } from '../ProjectDetail'; // Importa os dados
 
 const ListaProjetos = () => {
     // Array de Projetos
-    const listProjects = [
-        { name: 'EduCoChain', icon: educochain, date: '23 de junho de 2025' },
-        { name: 'SafeWater', icon: safewater, date: '10 de março de 2025' },
-        { name: 'Galeria Virtual', icon: galeriavirtual, date: '5 de fevereiro de 2025' },
-    ];
+    const listProjects = Object.entries(projectsData).map(([path, project]) => ({
+      name: project.name,
+      path: path,
+      icon: project.icon,
+      date: project.date,
+    }));
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-7xl px-4 md:px-6">
             {listProjects.map((project, index) => (
-                <div key={index} className="bg-neutral-900 rounded-xl shadow-lg transform transition-transform duration-300 hover:scale-105 overflow-hidden flex flex-col">
-                    <div className="flex justify-center items-center bg-gray-800 relative h-48 md:h-60">
-                        <img
-                            src={project.icon}
-                            alt={project.name}
-                            className="absolute inset-0 w-full h-full object-cover"
-                        />
+                // Link para que direcione
+                <Link key={index} to={`/projects/${project.path}`}>
+                    <div className="bg-neutral-900 rounded-xl shadow-lg transform transition-transform duration-300 hover:scale-105 overflow-hidden flex flex-col">
+                        {/* Imagem */}
+                        <div className="flex justify-center items-center bg-gray-800 relative h-48 md:h-60">
+                            <img
+                                src={project.icon}
+                                alt={project.name}
+                                className="absolute inset-0 w-full h-full object-cover"
+                            />
+                        </div>
+                        
+                        {/* Informações do projeto */}
+                        <div className="flex flex-col items-center text-center p-4 flex-grow">
+                            <h2 className="mt-0 mb-2 text-2xl font-semibold">{project.name}</h2>
+                            <span className="text-gray-400 text-sm">{project.date}</span>
+                        </div>
                     </div>
-                    
-                    {/* Informações do projeto */}
-                    <div className="flex flex-col items-center text-center p-4 flex-grow">
-                        <h2 className="mt-0 mb-2 text-2xl font-semibold">{project.name}</h2>
-                        <span className="text-gray-400 text-sm">{project.date}</span>
-                    </div>
-                </div>
+                </Link>
             ))}
         </div>
     );
