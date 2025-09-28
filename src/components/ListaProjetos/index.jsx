c308b4488fb92a3f8bfaaa93a709b47c27d7dc1b
Luiz <nandoluizprimeiro@gmail.com>
2025-09-27 22:14:20 -0300

    Style: Imagens cobrindo todo o card de projeto, adicionando imagens dos projetos

--- src/components/ListaProjetos/index.jsx
+++ src/components/ListaProjetos/index.jsx
@@ -1,27 +1,24 @@
-import nodejsIcon from '../../assets/icons/nodejs.svg';
-import javascriptIcon from '../../assets/icons/javascript.svg';
-import typescriptIcon from '../../assets/icons/typescript.svg';
-import pythonIcon from '../../assets/icons/python.svg';
+import educochain from '../../assets/project-img/educochain.png';
+import safewater from '../../assets/project-img/safewater.png';
+import galeriavirtual from '../../assets/project-img/galeriavirtual.png';
 
 const ListaProjetos = () => {
     // Array de Projetos
     const listProjects = [
-        { name: 'EduCoChain', icon: pythonIcon, date: '23 de junho de 2025' },
-        { name: 'Proza', icon: javascriptIcon, date: '15 de maio de 2025' },
-        { name: 'SafeWater', icon: typescriptIcon, date: '10 de março de 2025' },
-        { name: 'Galeria Virtual', icon: nodejsIcon, date: '5 de fevereiro de 2025' },
+        { name: 'EduCoChain', icon: educochain, date: '23 de junho de 2025' },
+        { name: 'SafeWater', icon: safewater, date: '10 de março de 2025' },
+        { name: 'Galeria Virtual', icon: galeriavirtual, date: '5 de fevereiro de 2025' },
     ];
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-7xl px-4 md:px-6">
             {listProjects.map((project, index) => (
                 <div key={index} className="bg-neutral-900 rounded-xl shadow-lg transform transition-transform duration-300 hover:scale-105 overflow-hidden flex flex-col">
-                    {/* Imagem */}
-                    <div className="flex justify-center items-center bg-gray-800 p-8 h-4/2">
+                    <div className="flex justify-center items-center bg-gray-800 relative h-48 md:h-60">
                         <img
                             src={project.icon}
                             alt={project.name}
-                            className="w-24 h-24 object-contain"
+                            className="absolute inset-0 w-full h-full object-cover"
                         />
                     </div>
                     
